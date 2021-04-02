dbc3dd4e4df0db60c61dae3638ad7db76d2d26a6
BM <sazya24@gmail.com>
2021-04-02 18:06:24 +0900

    init, destroy servelt

--- demo/src/main/java/com/weather/demo/MailService.java
+++ demo/src/main/java/com/weather/demo/MailService.java
@@ -1,5 +1,8 @@
 package com.weather.demo;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,15 +14,31 @@ public class MailService {
 	@Autowired
 	private JavaMailSender javaMailSender;
 
-	public void sendEmail(String emailAddr, String content) {
+	SimpleDateFormat timeFormat = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+	Calendar time = Calendar.getInstance();
+	String nowTime = timeFormat.format(time.getTime());
+	
+	public void sendStartEmail(String emailAddr, String content) {
+		
 		SimpleMailMessage mail = new SimpleMailMessage();
 		mail.setTo(emailAddr);
 		mail.setFrom("kbkbbitcamp@gmail.com");
-		mail.setSubject("[Service] Error Notify");
+		mail.setSubject("Service Start Notify" + nowTime);
 		mail.setText(content);
 
 		javaMailSender.send(mail);
 
+	}
+	
+	public void sendServerDownEmail(String emailAddr, String content) {
+		SimpleMailMessage mail = new SimpleMailMessage();
+		mail.setTo(emailAddr);
+		mail.setFrom("kbkbbitcamp@gmail.com");
+		mail.setSubject("Error Notify [Service Down]" + nowTime);
+		mail.setText(content);
+		
+		javaMailSender.send(mail);
+		
 	}
 
 }
