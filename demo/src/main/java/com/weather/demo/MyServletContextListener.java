eadb2adf6581ed61857c03b3c2d77cfbe874385a
BM <sazya24@gmail.com>
2021-04-02 18:07:10 +0900

    Update MyServletContextListener.java

--- demo/src/main/java/com/weather/demo/MyServletContextListener.java
+++ demo/src/main/java/com/weather/demo/MyServletContextListener.java
@@ -1,5 +1,17 @@
-package com.weather.demo;
-
-public class MyServletContextListener {
-
-}
+//package com.weather.demo;
+//
+//import javax.servlet.ServletContextListener;
+//
+//public class MyServletContextListener implements ServletContextListener {
+//	
+//	@Override
+//    public void contextInitialized(ServletContextEvent event) {
+//        System.out.println("psy tomcat Start!!!");
+//        
+//    }
+//    
+//    @Override
+//    public void contextDestroyed(ServletContextEvent event) {
+//    }
+//
+//}
